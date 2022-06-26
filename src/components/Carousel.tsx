471c3ac4665434c067d5c69cd87631bf64fa6e51
Robert Czajka <robchike9290@gmail.com>
2022-06-26 17:46:54 -0500

    Create anmd implement CardNums interface.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,22 +2,28 @@ import React, {useEffect, useState, useCallback} from 'react';
 import Card from './Card';
 import cardData from '../cardData';
 
-const Carousel = () => {
-  // const [cardRefs, setCardRefs] = useState({
-  //   previousCard: 0,
-  //   currentCard: 0,
-  //   nextCard: 1
-  // })
+interface CardNums {
+  previousCard: number;
+  currentCard: number;
+  nextCard: number;
+}
 
-  // const setCardStates = (cards, cardId) => {
-  //   if (cards.currentCard === cardId) {
-  //     return 'currentCard';
-  //   } else if (cards.previousCard === cardId) {
-  //     return 'previousCard';
-  //   } else if (cards.nextCard === cardId) {
-  //     return 'nextCard';
-  //   }
-  // }
+const Carousel = () => {
+  const [cardRefs, setCardRefs] = useState<CardNums>({
+    previousCard: 0,
+    currentCard: 0,
+    nextCard: 1
+  })
+
+  const setCardStyles = (cardRefs: CardNums, cardId: number) => {
+    if (cardRefs.currentCard === cardId) {
+      return 'currentCard';
+    } else if (cardRefs.previousCard === cardId) {
+      return 'previousCard';
+    } else if (cardRefs.nextCard === cardId) {
+      return 'nextCard';
+    }
+  }
 
   return (
     <div>
